d44af87bee5a7858c40656419b2a49bf88a6d1c9
codingnooooob <mountaindew0812@gmail.com>
2019-12-13 23:14:48 +0900

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: [:edit, :update]
-  resources :groups, except: [:new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
   # resources :messages, only: [:new, :create, :show, destroy]
 end
