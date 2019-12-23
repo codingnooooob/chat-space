8873447e5f084facb0aba967a024f6b46126a6a2
codingnooooob <mountaindew0812@gmail.com>
2019-12-23 14:28:01 +0900

    update_create_messages.rb

--- db/migrate/20191215170242_create_messages.rb
+++ db/migrate/20191215170242_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.integer :id, null:false
+      # t.integer :id, null:false
       t.text :text
       t.string :image
       t.references :user, foreign_key: true
